962fbb1695c9d035ef0b30ddf0b84a249cafef17
Dandav2923 <daniele.emili23@gmail.com>
2022-01-17 17:28:36 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,22 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header />
+    <Main />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Header,
+    Main,
   },
 };
 </script>
