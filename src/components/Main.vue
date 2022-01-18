61ad4edf8252c40a3377c4a42875350fbdc060f1
Dandav2923 <daniele.emili23@gmail.com>
2022-01-18 16:57:10 +0100

    prova selezione valori della select

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,15 +11,19 @@
         />
     </div>
     <div class="row p-4">
-      <select id="select" 
-              class="form-select" 
-              aria-label="Default select example">
-        <option selected>Seleziona il tuo genere preferito</option>
-        <option value="rock">Rock</option>
-        <option value="pop">Pop</option>
-        <option value="jazz">Jazz</option>
-        <option value="metal">Metal</option>
-      </select>
+      <Search 
+        @searchAlbum="searchAlbumFilter($event.target.value)"
+         />
+      <!-- <select 
+        id="select" 
+        class="form-select" 
+        aria-label="Default select example">
+          <option selected>Seleziona il tuo genere preferito</option>
+          <option value="rock">Rock</option>
+          <option value="pop">Pop</option>
+          <option value="jazz">Jazz</option>
+          <option value="metal">Metal</option>
+      </select> -->
     </div>
   </div>
 </template>
@@ -27,15 +31,18 @@
 <script>
 import axios from "axios";
 import Album from "./Album.vue";
+import Search from "./Search.vue"
 
 export default {
   name: "Main",
   components:{
     Album,
+    Search,
   },
   data() {
     return {
       album: null,
+      filteredGenre:'',
     }
   },
   created() {
@@ -49,6 +56,49 @@ export default {
         console.log(error);
         });
   },
+  methods: {
+    searchAlbumFilter(inputValue) {
+      this.filteredGenre = inputValue;
+    },
+    filteredArray(){
+      console.log('1');
+      // if(this.filteredGenre == 'selected' ){
+      //   return this.album
+      // }
+      // else if (this.filteredGenre == 'rock') {
+      //   return this.album.filter((element) => element.genre == 'Rock');
+      // }
+      // else if (this.filteredGenre == 'pop') {
+      //   return this.album.filter((element) => element.genre == 'Pop');
+      // }
+      // else if (this.filteredGenre == 'jazz') {
+      //   return this.album.filter((element) => element.genre == 'Jazz');
+      // }
+      // else if (this.filteredGenre == 'metal') {
+      //   return this.album.filter((element) => element.genre == 'Metal');
+      // }
+    }
+  },
+  // computed:{
+  //   filteredArray(event){
+  //     console.log(event.target.value);
+  //     // if(this.filteredGenre == 'selected' ){
+  //     //   return this.album
+  //     // }
+  //     // else if (this.filteredGenre == 'rock') {
+  //     //   return this.album.filter((element) => element.genre == 'Rock');
+  //     // }
+  //     // else if (this.filteredGenre == 'pop') {
+  //     //   return this.album.filter((element) => element.genre == 'Pop');
+  //     // }
+  //     // else if (this.filteredGenre == 'jazz') {
+  //     //   return this.album.filter((element) => element.genre == 'Jazz');
+  //     // }
+  //     // else if (this.filteredGenre == 'metal') {
+  //     //   return this.album.filter((element) => element.genre == 'Metal');
+  //     // }
+  //   }
+  // },
 };
 </script>
 
---
+++ src/components/Search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <select 
+        id="select" 
+        class="form-select" 
+        aria-label="Default select example"
+        v-model="inputValue"
+        @change="$emit('searchAlbum', $event.target.value)">
+          <option selected>Seleziona il tuo genere preferito</option>
+          <option :value="Rock">Rock</option>
+          <option :value="Pop">Pop</option>
+          <option :value="Jazz">Jazz</option>
+          <option :value="Metal">Metal</option>
+      </select>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Search',
+    props: {
+        value: String,
+    },
+
+    data() {
+        return {
+            inputValue: '',
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
